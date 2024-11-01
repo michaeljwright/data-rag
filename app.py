81c9a878023c94a99b6d8600a943c8c8780b9371
Mike Wright <michaeljwright@users.noreply.github.com>
2024-11-01 21:29:38 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st 
+from langchain_community.llms import Ollama 
+import pandas as pd
+from pandasai import SmartDataframe
+
+llm = Ollama(model="llama3.1")
+
+st.title("Experiment: Chat with Data")
+st.subheader("RAG: Retrieval Augmented Generation")
+
+uploader_file = st.file_uploader("Upload a CSV file", type= ["csv"])
+
+if uploader_file is not None:
+    data = pd.read_csv(uploader_file)
+    st.write(data.head(3))
+    df = SmartDataframe(data, config={"llm": llm, "verbose": True})
+    prompt = st.text_area("Enter your prompt:")
+
+    if st.button("Generate"):
+        if prompt:
+            with st.spinner("Generating response..."):
+                st.write(df.chat(prompt))
+        else:
+            st.warning("Please enter a prompt!")
